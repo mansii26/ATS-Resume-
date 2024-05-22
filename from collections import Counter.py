76d6897c7c661363543540443dc810798d6fccd8
mansii26 <im.mansi26@gmail.com>
2024-05-22 19:09:51 +0530

    ATS

---
+++ from collections import Counter.py
@@ -0,0 +1,13 @@
+
+from collections import Counter
+
+X=int(input("no of shoes"))
+
+size=[]
+
+for i in range(X):
+    s=float(input("enter the size of shoes"))
+    size.append(s)
+print(size)
+
+N=int(input("no of customers")) 
